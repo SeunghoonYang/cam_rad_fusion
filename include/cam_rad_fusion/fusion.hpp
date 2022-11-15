95685a139e935b81e803518dbab4fc9e814c2cc5
SeunghoonYang <shyang9512@kaist.ac.kr>
2022-11-15 20:35:39 +0900

    opencv

--- include/cam_rad_fusion/fusion.hpp
+++ include/cam_rad_fusion/fusion.hpp
@@ -21,9 +21,14 @@ private:
     typedef message_filters::Synchronizer<MySyncPolicy> Sync;
     boost::shared_ptr<Sync> sync_;
 
+    // publisher
+    image_transport::ImageTransport it_;
+    image_transport::Publisher image_pub;
+
+
 public:
     
-    FUSION()
+    FUSION() : it_(nh)
     {
         // params.yaml 
         nh.param<std::string>("cam_rad_fusion/imageTopic", image_topic, "/camera_array/cam0/image_raw");
@@ -36,6 +41,9 @@ public:
         radar_sub.subscribe(nh, radar_topic, 1);
         sync_.reset(new Sync(MySyncPolicy(10), image_sub, lidar_sub, radar_sub));
         sync_->registerCallback(boost::bind(&FUSION::callback, this, _1, _2, _3));
+
+        image_pub = it_.advertise("/FUSION/image_out", 1);
+
     }
 
     void callback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::PointCloud2ConstPtr& lidar, const sensor_msgs::PointCloud2ConstPtr& radar)
@@ -45,7 +53,20 @@ public:
         std::cout << "Your lidar time : " << lidar->header.stamp.sec << lidar->header.stamp.nsec << std::endl;  
         std::cout << "Your radar time : " << radar->header.stamp.sec << radar->header.stamp.nsec << std::endl;
 
+        // change format  ROS -> OpenCV
+        cv_bridge::CvImagePtr cv_ptr;
+        cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+        cv::Mat image_cv = cv_ptr->image;
+
+        // TODO!!!!!!
+        // use opencv function with "image_cv"
+        cv::rectangle(image_cv, cv::Rect(50, 50, 100, 50), cv::Scalar(0, 0, 255), 2);
+
+        // change format  OpenCV -> ROS
+        sensor_msgs::ImagePtr image_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image_cv).toImageMsg();
+        image_pub.publish(image_msg);
     }
+
 
 };
 
--- include/cam_rad_fusion/global_header.hpp
+++ include/cam_rad_fusion/global_header.hpp
@@ -13,6 +13,11 @@
 
 
 // OpenCV
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 
 // PCL
